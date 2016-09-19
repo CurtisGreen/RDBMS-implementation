29b68fe09319e0e25cded24a33ae6a2c0f77acea
Jose Ramos <joseramos@dhcp-10-202-145-178.tamulink.tamu.edu>
2016-09-19 13:53:55 -0500

    create main

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//database header
+
+#ifndef _databaseh_
+#define _databaseh_
+
+#include <vector>
+#include <iostream>
+
+class table{
+
+ 
+  std::string name;
+  std::vector<std::string> attributes;
+
+};
+
+#endif
