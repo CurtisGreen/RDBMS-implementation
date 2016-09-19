51f1e1ddd20a433b7fcee1b8abfecbf6aefb31ab
greencur000 <greencur000@users.noreply.github.tamu.edu>
2016-09-19 11:14:26 -0500

    Update database.h

--- database.h
+++ database.h
@@ -1,4 +1,8 @@
 //database header
+
+#ifndef _database.h_
+#define _database.h_
+
 #include <vector>
 #include <iostream>
 
@@ -6,3 +10,5 @@ class table{
   std::vector<std::string> headers;
   std::vector<std::vector<std::string>> data;
 };
+
+#endif
