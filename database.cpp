cfc3c1a185b27c58582f41ce4633744f8e41f688
inga-irving80 <inga-irving80@users.noreply.github.tamu.edu>
2016-09-19 15:03:41 -0500

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -4,3 +4,10 @@ void database::create(string name, vector<Attribute> att, vector<string> key){
 }
 void database::insert(){
 }
+ 
+ void database :: destroy(){
+ }
+ }
+ void database :: update(){
+   
+ }
