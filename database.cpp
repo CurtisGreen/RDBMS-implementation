0acbb289ecd80d5a36269b4f1dbc074e7e170cbd
Jose Ramos <joseramos@dhcp-10-201-144-36.tamulink.tamu.edu>
2016-09-20 11:17:31 -0500

    added getter to attributes and did minor changes

--- database.cpp
+++ database.cpp
@@ -6,58 +6,64 @@
 #include "database.h"
 
 //Database cpp
-<<<<<<< HEAD
-void Engine::open(){
+
+void Engine::open(string table_name){
 	//TODO
 }
-void Engine::close(){
+void Engine::close(string table_name){
 	//TODO
 }
-void Engine::write(){
-=======
-void Database::open(string table_name){
-	//TODO
-}
-void Database::close(string table_name){
-	//TODO
-}
-void Database::write(Table table){
->>>>>>> f659fbb3ee6b8babdbb19d5a36bfebcf55d01a73
+void Engine::write(Table table){
+
 	//TODO
 }
 void Engine::exit(){
 	//TODO
 }
-<<<<<<< HEAD
-void Engine::show(){
-=======
-void Database::show(string table_name){
->>>>>>> f659fbb3ee6b8babdbb19d5a36bfebcf55d01a73
+
+void Engine::show(string table_name){
+
 	//TODO
 }
 
 void Engine::create(string name, vector<Attribute> att, vector<string> key){
 
 	Table* table = new Table(name,att,key);
-	tables.push_back(table);
+	all_tables.push_back(table);
 }
 
 
 void Engine::update(){
 	//TODO
 }
+
+///----Jose---I commented this funciton because it has a few erros. name is priviate.
+///--I think == is not use to compare strings.  I think type[] is used wrong 
 void Engine::insert(string name, vector<Attribute> att){
 	//TODO
+	//Retrieve table from memory or file using 'name'
+	//Table table;	//For right now just create one, will change later TODO
+/*
+	for (int i = 0; i < table.attributes.size(); i++){	//Loop through comparing header & data b/c data might not be in the right order
+		for (int k = 0; k < att.size(); k++){
+			if (att[i].name == table.attributes[k] && att[i].type == table.type[k]){
+				table.attributes[k].push_back(att[i].data[0]);
+			}
+		}
+	}
+	*/
 }
 void Engine::destroy(){
 	//TODO
 }
-void Database :: drop(string table_name){
+void Engine::drop(string table_name){
+	/*
 	for(int i = 0; i < all_tables.size(); i++){
 		if (all_tables[i].name == table_name){
 			all_tables.erase(all_tables.begin() + i);
 		}
 	}
+	*/
 }
 //TODO part 2: atomic expressions
 // selection-projection-renaming-union-difference-product-natural-join
