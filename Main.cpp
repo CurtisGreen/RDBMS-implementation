3d6dc623914a5ccdb0fcab02bed982542eec1242
Jose Ramos <joseramos@dhcp-10-202-144-181.tamulink.tamu.edu>
2016-09-21 12:33:58 -0500

    added data to main

--- Main.cpp
+++ Main.cpp
@@ -15,8 +15,42 @@ int main(){
 
 	Engine e; 
 
+	vector<string> name = {"Peter Parker","Andrew Wills","Antonio Ramos"};
+	vector<string> ocupataion = {"students","Proffesor","Boxer"};
+	vector<string> weight = {"150","160","250"};
+	vector<string> height = {"5","10","8"};
+
+ 
+
+	vector<string> hname = {"Spider Man","Super Man","Hulk"};
+	vector<string> hability = {"Strenght","Fly","Strenght"};
+	vector<string> hweight = {"150","160","250"};
+	vector<string> hheight = {"5","10","8"};
+
+
+	vector<string> universe = {"Earth","Mars","Earth"};
+	vector<string> purpose = {"life saver","life saver","life saver"};
+	vector<string> gname = {"Marvel","DC","Marvel"};
+
+
+	Attribute att_human_name("Name","string",name);
+	Attribute att_human_name("ocupation","string",name);
+	Attribute att_human_name("weight","string",name);
+	Attribute att_human_name("Name","string",name);
+
+	
+	
+
+
 
 	Attribute test_attribute;
+
+
+
+
+
+
+
 
 	cout << "This is just a test ";
 	/// table.create("SuperHero", "test_attribute",  );
