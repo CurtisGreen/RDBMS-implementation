146d51b1adb35e4c87751621609001cc3aa5ca42
inga-irving80 <inga.irving80@linux2.cs.tamu.edu>
2016-09-29 00:39:47 -0500

    Update Parser.h

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -36,46 +36,41 @@ public: // If you are trying to get a token use Parser:: Token
 		vector <Parser::Token> getToken();
 
 private:
-		vector<Parser::Token> tokens; 
-		Token token;
-	    Engine e;
+	 vector<Parser::Token> tokens; 
+	 Token token;
+	 Engine e;
 		
-		int current_token_index; // current place in token vector
+	int current_token_index; // current place in token vector
 	  
-	    void setToken();
-	    void increment(); // go through vector to get next item 
+	void setToken();
+	void increment(); // go through vector to get next item 
 		
-	    bool can_execute_program(); // calls command or query functions
-	    bool command();  // calls all of the different command functions 
-	    bool query();	// calls all of the different query-like functions
+	bool can_execute_program(); // calls command or query functions
+	bool command();  // calls all of the different command functions 
+	bool query();	// calls all of the different query-like functions
 		
-		// Execute all of the commands depending on what is called
-	    Table execute_expression();
-	    Table atomic_expression();
-	    Table execute_selection();
-	    Table execute_projection();
-	    Table execute_renaming();
-		void execute_exit();
-		void execute_show();
-		void execute_open();
-		void execute_write();
-		void execute_close();
-	    void execute_create();
-	    void execute_update();
-	    void execute_insert();
-	    void execute_destroy();
+	// Execute all of the commands depending on what is called
+	Table execute_expression();
+	Table atomic_expression();
+	Table execute_selection();
+	Table execute_projection();
+	Table execute_renaming();
+	void execute_exit();
+	void execute_show();
+	void execute_open();
+	void execute_write();
+	void execute_close();
+	void execute_create();
+	void execute_update();
+        void execute_insert();
+	void execute_destroy();
 
 
-	    // need a helper function to get all of the attributes for create function
-	    vector<string> attribute_list();
+	// need a helper function to get all of the attributes for create function
+	vector<string> attribute_list();
 		
-		// helper function in the Parser that allows setting up a format of entry for commands
-	    void expect(Token token);
+	// helper function in the Parser that allows setting up a format of entry for commands
+	void expect(Token token);
 	
-	  
-
-	
-
-
 };
 #endif
