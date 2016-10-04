8c485192df01e8e36ac9314bcab136bac604cc0d
Inga Irving <inga.irving80@linux2.cs.tamu.edu>
2016-10-04 00:09:41 -0500

    adding interactive system

---
+++ Marvel.h
@@ -0,0 +1,25 @@
+#ifndef MARVEL_H
+#define MARVEL_H
+
+
+class Marvel
+{
+	
+public:
+	
+	void initialize();
+	void marvel_menu();
+	
+	void update_info();
+	void find_character();
+	void create_character();
+	void delete_character();
+	void show_characters();
+	
+	void quit_app();
+	
+	
+	
+
+};
+#endif
