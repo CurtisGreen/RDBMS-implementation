7a14d51b8d9d46bdde611576f9fe4efa12d24135
Inga Irving <inga.irving80@linux2.cs.tamu.edu>
2016-10-04 00:10:03 -0500

    adding interactive system

---
+++ Marvel.cpp
@@ -0,0 +1,50 @@
+#include "Marvel.h"
+
+
+using namespace std;
+
+
+
+void  Marvel :: initialize()
+{
+	//TODO :
+	//Create tables for the user
+}
+void  Marvel :: marvel_menu()
+{
+	//TODO
+	//Tells the user all of the statements that the user can request 
+}
+void Marvel :: update_info()
+{
+	//TODO:
+	//Updates information in the database 
+}
+void Marvel :: find_character()
+{
+	//TODO:
+	//Performs searches on the database 
+}
+	
+void Marvel :: create_character()
+{
+	//TODO:
+	//Receive the Information to allow  a creation of a character
+}
+void Marvel :: delete_character()
+{
+	//TODO:
+	//Allows the user to delete characters that they have created 
+}
+void Marvel :: show_characters()
+{
+	//TODO:
+	//Shows all of the character information in the database
+}
+void Marvel :: quit_app()
+{
+	//TODO:
+	//Quits the database 
+}
+	
+	
