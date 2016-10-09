83238f6052198735053470d7fe626d7eaef16ed9
Jose Ramos <joseramos@dhcp-10-202-145-98.tamulink.tamu.edu>
2016-10-09 11:12:01 -0500

    client server

--- Application/Marvel.h
+++ Application/Marvel.h
@@ -1,18 +1,15 @@
 #ifndef MARVEL_H
 #define MARVEL_H
 
-#include "Parser.h"
-
 
 using namespace std;
+#include <iostream>
 
 class Marvel
 {
 
 public: 
 	Marvel(){};
-	Marvel(Parser p_ ){p = p_;}
-	Parser p;
 	void Menu();
 	void update_info();
 	void rename();
